d661f5b808ded954065ce70006c72410cef7a57b
MGondolesi <mgondolesi@gmail.com>
2019-08-19 18:44:03 -0300

    servidor localhost

--- app/src/providers/api/api.ts
+++ app/src/providers/api/api.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
  */
 @Injectable()
 export class Api {
-  url: string = 'https://pet-place.herokuapp.com/api';
+  url: string = 'http://localhost:5000/api';
 
   constructor(public http: HttpClient) {
   }
