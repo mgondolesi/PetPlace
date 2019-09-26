a7c0c4a192a70020f3bfc7223f00dc0a31a6ebb6
julianutn <juliancai94@gmail>
2019-09-26 11:35:06 -0300

    intento de mostrar los datos de usuario en welcome

--- src/app/pages/welcome/welcome.page.ts
+++ src/app/pages/welcome/welcome.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-welcome',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomePage implements OnInit {
 
-  constructor() { }
+  //data: any;
+  data: 'usuario';
+
+  constructor(private storage: Storage) {
+  
+    
+    }
 
   ngOnInit() {
-  }
+
+   
+      this.storage.get('usuario').then((val) => {
+        this.data = val;
+      }).catch((error) => {
+        console.log('get error for ' , error);
+      });
+    }
+  
 
 }
