0f1a6be61109dca5e4147b6fa7bc56472811b9ad
DAMoreira <sk4rfac3@gmail.com>
2019-10-07 00:10:45 -0300

    cosas

---
+++ src/app/mascota.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { URL } from '../app/config';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MascotaService {
+
+  constructor(public http: HttpClient) { }
+
+
+  registrarMascota(datos) : Observable<any>{                           //metodo que ejecuta POST registerUser de la api con los "datos" desde el metodo register(form) de regist.page
+    return this.http.post(URL+"registerM", datos);               // el return devuelve lo que me mando la api (usuario y token)
+  }
+
+
+}
--- src/app/pages/create-pet/create-pet.page.ts
+++ src/app/pages/create-pet/create-pet.page.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController, NavController, ToastController,  AlertController } from '@ionic/angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+import { Router } from '@angular/router';
+
+import { NgForm } from '@angular/forms';
+import { MascotaService } from 'src/app/mascota.service';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { Storage } from '@ionic/storage';
+import { LoadingService } from 'src/app/loading.service';
 
 @Component({
   selector: 'app-create-pet',
@@ -7,9 +18,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePetPage implements OnInit {
 
-  constructor() { }
-
+  form: NgForm;
+  formularioRegistro: FormGroup;
+  constructor(
+    private authService: MascotaService,
+    public router: Router, 
+    public formBuilder: FormBuilder,
+    private storage: Storage,
+    public toastController: ToastController,
+    public alertController: AlertController,
+    public loading: LoadingService,
+  ) {  }
   ngOnInit() {
+  }
+  // Dismiss Register Modal
+  
+  
+  register(form) {
+    this.loading.present();
+    this.authService.registrarMascota(form.value).subscribe(            //Ejecuta el metodo registrar(datos) de usuario.service y le manda los datos del form (que serian "form.value")
+      data => {                                         //Si la api devuelve data almacena los datos en SQLite
+
+          this.router.navigate(['/welcome']);
+          
+          this.toastController.create({
+            message: data.msj,
+            duration: 3000
+          }).then((toastData)=>{
+            console.log(toastData);
+            toastData.present();
+          }); 
+
+          this.loading.dismiss();
+
+         
+      
+      },
+      error => {  
+                               
+        console.log(error);                                //Si la api devuelve error almacena los datos en SQLite
+          
+          this.toastController.create({
+            message: error.error.msg,
+            duration: 3000
+          }).then((toastData)=>{
+            console.log(toastData);
+            toastData.present();
+          }); 
+          this.loading.dismiss();  
+      },
+      () => {
+        
+      }
+    );
   }
 
 }
+
+
