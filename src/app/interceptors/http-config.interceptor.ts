70cfd81a7df679a0ab8faeea9ef056bdf042fcef
julianutn <juliancai94@gmail>
2019-10-11 12:04:21 -0300

    redirecciona al home y otras weas
    -redireccion al home
    - loading controller en interceptor
    -toast inicie sesion en interceptor

--- src/app/interceptors/http-config.interceptor.ts
+++ src/app/interceptors/http-config.interceptor.ts
@@ -10,12 +10,19 @@ import {
   import { Observable, throwError, from} from 'rxjs';
   import { map, catchError, switchMap } from 'rxjs/operators';
   import { Injectable } from '@angular/core';
+  import { Router } from '@angular/router';
+  import { ToastController } from '@ionic/angular';
+  import { LoadingService } from 'src/app/loading.service';
+
   const TOKEN_KEY = 'token';
   @Injectable()
   export class HttpConfigInterceptor implements HttpInterceptor {
     
     constructor(
       public storage: Storage,
+      private router: Router, 
+      public toastController: ToastController,
+      public loading: LoadingService,
     ) { }
     intercept(request: HttpRequest<any>, 
                  next: HttpHandler): 
@@ -29,19 +36,38 @@ import {
                 headers: request.headers.set('Authorization','bearer '+token) 
               });
             }
-            
+            this.loading.present();
             return next.handle(request).pipe(
               map((event: HttpEvent<any>) => {
                 if (event instanceof HttpResponse) {
+                  this.loading.dismiss()
                 }
+                this.loading.dismiss()
                 return event;
               }),
+              
               catchError((error: HttpErrorResponse) => {
+                if (error.status === 401) {
+                  this.loading.dismiss()
+                  this.router.navigate(['/home']);
+                  this.toastController.create({
+                    message: "Por favor, inicie sesion",
+                    duration: 3000
+                  }).then((toastData)=>{
+                    console.log(toastData);
+                    toastData.present();
+                  });
+                }
                 console.error(error);
                 return throwError(error);
+               
               })
             );
+            
           })
+          
         );
     }
   }
+  
+ 
