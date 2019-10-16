0c2bccbd4ced8017c670953e19bff729a282beef
MGondolesi <mgondolesi@gmail.com>
2019-10-16 20:53:23 -0300

    edit-pet page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ const routes: Routes = [
   { path: 'welcome', loadChildren: './pages/welcome/welcome.module#WelcomePageModule' },  { path: 'create-pet', loadChildren: './pages/create-pet/create-pet.module#CreatePetPageModule' },
   { path: 'my-pets', loadChildren: './pages/my-pets/my-pets.module#MyPetsPageModule' },
   { path: 'logout', loadChildren: './pages/logout/logout.module#LogoutPageModule' },
+  { path: 'edit-pet', loadChildren: './pages/edit-pet/edit-pet.module#EditPetPageModule' },
 
 
 
