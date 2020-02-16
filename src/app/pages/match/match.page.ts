a50374072d9a3d99569f8fe3227e503e04b8bb9b
SantiagoAguilar <santiaguilar688@gmail.com>
2020-02-16 19:35:52 -0300

    asda

--- src/app/pages/match/match.page.ts
+++ src/app/pages/match/match.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MatchService } from 'src/app/services/match.service';
 import { MatchFunctionsService } from 'src/app/services/match-functions.service'
+import { MascotaService } from 'src/app/services/mascota.service';
+import { Storage } from '@ionic/storage';
+import { Mascota } from 'src/model/mascota.model';
 
 @Component({
   selector: 'app-match',
@@ -11,16 +14,27 @@ export class MatchPage implements OnInit {
 
   matches: any[] = [];
   pendientes: any[] = [];
+  userId:string;
+  mascotas : Mascota[] =[];
 
   constructor(private matchService: MatchService,
-              private matchServiceFunctions: MatchFunctionsService,) { }
+              private matchServiceFunctions: MatchFunctionsService,
+              private storage: Storage,
+              private mascotaService: MascotaService) { }
 
   ngOnInit() {
+
+    this.storage.get('usuario').then((val) => {                    //como en el login guarde los datos de usuario, obtengo el "username"
+      this.userId = val._id;
+    }).catch((error) => {
+      console.log('get error for ', error);
+    });
+
   
     this.matchService.misMatches()  //los aceptados
       .subscribe(
         (data) => { // Success
-          this.matches = data['match'];
+          // this.matches = data['match'];
           console.log(data);
         },
         (error) => {
@@ -38,6 +52,32 @@ export class MatchPage implements OnInit {
           console.error(error);
         }
       )
+
+      this.matchService.todosMisMatches().subscribe((res)=>{
+        console.log(res)
+        this.matches = res['match'];
+        this.matches.forEach((elem)=>{
+          let mascotaEmi = new Mascota;
+          let mascotaRece = new Mascota(res.mascotaRece);
+          console.log(elem.mascotaEmi,elem.mascotaRece)
+          this.mascotaService.getMascotaByID(mascotaRece).subscribe((res)=>{
+          
+            console.log(res)
+            Object.assign(elem,{nombreReceptor:res.nombre});
+          },(err)=>{
+            console.log(err)
+          })
+          this.mascotaService.getMascotaByID(mascotaEmi).subscribe((res)=>{
+            Object.assign(elem,{nombreEmisor:res.nombre});
+          },(err)=>{
+            console.log(err)
+          })
+        })
+        console.log(this.matches)
+      },(err)=>{
+        console.log(err)
+      }
+      )
   }
  
   aceptar(match){
@@ -45,6 +85,8 @@ export class MatchPage implements OnInit {
     this.matchServiceFunctions.aceptar(match);  //aceptar el match
     
   }
+
+  
 
   rechazar(match){
         
--- src/model/mascota.model.ts
+++ src/model/mascota.model.ts
@@ -1,13 +1,18 @@
-export interface Mascota{
-    _id : String,
-    nombre : String,
-    raza : String,
-    sexo : String,
-    nroPariciones : String,
-    fNacimiento : String,
-    foto : String,
-    ubicacion : String,
-    descripcion: String,
-    pedigree : Boolean,
-    amo : String,
+export class Mascota{
+    _id : String;
+    nombre : String;
+    raza : String;
+    sexo : String;
+    nroPariciones : String;
+    fNacimiento : String;
+    foto : String;
+    ubicacion : String;
+    descripcion: String;
+    pedigree : Boolean;
+    amo : String;
+
+    constructor( id: string) {
+        this._id =  id;
+    }
+    
 }
